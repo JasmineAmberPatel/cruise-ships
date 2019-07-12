bb0bc5df644c26f302abb655f37680fe5b7a412b
Jasmine Amber <jasmineamber.ja@gmail.com>
2019-07-12 14:17:47 +0100

    dryed up port tests

--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -1,26 +1,29 @@
 const Port = require('../src/port.js');
 
-describe('constructor', () => {
-  it('returns an object', () => {
-    expect(new Port()).toBeInstanceOf(Object);
+describe('Port object', () => {
+  let port;
+  beforeEach(() => {
+    port = new Port('Portchester');
   });
-  it('sets the name property', () => {
-    const port = new Port('Portchester');
-    expect(port.name).toEqual('Portchester');
-  });
-  it('Adds a ship to the ships array', () => {
-    const port = new Port('Portchester');
-    const ship = {};
-    port.addShip(ship);
-    expect(port.ships).toContain(ship);
-  });
-  it('Removes a ship from the ships array', () => {
-    const port = new Port('Portchester');
-    const titanic = {};
-    const queenMary = {};
-    port.addShip(titanic);
-    port.addShip(queenMary);
-    port.removeShip(queenMary);
-    expect(port.ships).toEqual([titanic]);
+  describe('constructor', () => {
+    it('returns an object', () => {
+      expect(new Port()).toBeInstanceOf(Object);
+    });
+    it('sets the name property', () => {
+      expect(port.name).toEqual('Portchester');
+    });
+    it('Adds a ship to the ships array', () => {
+      const ship = {};
+      port.addShip(ship);
+      expect(port.ships).toContain(ship);
+    });
+    it('Removes a ship from the ships array', () => {
+      const titanic = {};
+      const queenMary = {};
+      port.addShip(titanic);
+      port.addShip(queenMary);
+      port.removeShip(queenMary);
+      expect(port.ships).toEqual([titanic]);
+    });
   });
 });
