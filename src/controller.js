efcd5ccf73258f07aadc16ce70699869869199f4
Jasmine Amber <jasmineamber.ja@gmail.com>
2019-08-01 15:54:50 +0100

    added ports

--- src/controller.js
+++ src/controller.js
@@ -3,17 +3,32 @@
     this.initialiseSea();
   }
 
-  Controller.prototype.initialiseSea = function initialiseSea() {
-    const backgrounds = [
-      './images/water0.png',
-      './images/water1.png',
-    ];
-    let backgroundIndex = 0;
-    window.setInterval(() => {
-      document.querySelector('#viewport').style.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
-      backgroundIndex += 1;
-    }, 1000);
-  };
+  Controller.prototype = {
+    initialiseSea: function () {
+      const backgrounds = [
+        './images/water0.png',
+        './images/water1.png',
+      ];
+      let backgroundIndex = 0;
+      window.setInterval(() => {
+        document.querySelector('#viewport').style.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
+        backgroundIndex += 1;
+      }, 1000);
+    },
+    renderPorts: function (ports) {
+      const portsElement = document.querySelector('#ports');
+      portsElement.style.width = '0px';
+      ports.forEach((port, index) => {
+        const newPortElement = document.createElement('div');
+        newPortElement.dataset.portName = port.name;
+        newPortElement.dataset.portIndex = index;
+        newPortElement.className = 'port';
+        portsElement.appendChild(newPortElement); 
+        const portsElementWidth = parseInt(portsElement.style.width, 10);
+        portsElement.style.width = `${portsElementWidth + 256}px`;   
+      })
+    },
+  }
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Controller;
   } else {
